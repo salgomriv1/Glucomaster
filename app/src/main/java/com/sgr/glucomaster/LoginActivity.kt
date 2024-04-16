5f754fb7bb41be002aeedf57f82c10de6b20e26b
salgomriv1 <salgomriv@alu.edu.gva.es>
2024-04-16 15:56:43 +0200

    Implemented Login Designed Register screen Implemented Register screen

--- app/src/main/java/com/sgr/glucomaster/LoginActivity.kt
+++ app/src/main/java/com/sgr/glucomaster/LoginActivity.kt
@@ -1,20 +1,41 @@
 package com.sgr.glucomaster
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.sgr.glucomaster.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    //Variables
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        //Using a splashScreen
         val screenSplash = installSplashScreen()
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        //Binding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,5 +44,106 @@ class LoginActivity : AppCompatActivity() {
 
         Thread.sleep(1000)
         screenSplash.setKeepOnScreenCondition {false}
+
+        //Variable init
+        auth = FirebaseAuth.getInstance()
+
+        //Check if there is a logged user
+      /*  if (auth.currentUser != null) {
+            val intent = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }*/
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.btnGLogin.setOnClickListener {
+            signInGoogle()
+        }
+
+        binding.btnIniciarSesion.setOnClickListener {
+            if (binding.etLoginMail.text.isNotEmpty() && binding.etLoginPass.text.isNotEmpty()) {
+                signIn(binding.etLoginMail.text.toString(), binding.etLoginPass.text.toString())
+            } else {
+                Toast.makeText(baseContext, getString(R.string.loginEnBlanco), Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.tvRegLink.setOnClickListener{
+
+            val intent = Intent (this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    //AUTH GOOGLE
+    //Function to sign in with Google
+    private fun signInGoogle() {
+
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+        if ( result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+
+        if (task.isSuccessful) {
+            val account : GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        } else {
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener {
+                if(it.isSuccessful) {
+                    val intent: Intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    //END GOOGLE AUTH
+
+    //Function to log in with email and pass
+    private fun signIn(email: String, pass: String) {
+
+        auth.signInWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) {
+
+                if (it.isSuccessful) {
+
+                    val user = auth.currentUser
+                    Toast.makeText(baseContext, user?.email + getString(R.string.authOK), Toast.LENGTH_LONG)
+                        .show()
+                    val intent = Intent (this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+
+                    Toast.makeText(baseContext, getString(R.string.errorLog), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
     }
 }
--- app/src/main/java/com/sgr/glucomaster/RegisterActivity.kt
+++ app/src/main/java/com/sgr/glucomaster/RegisterActivity.kt
@@ -1,20 +1,100 @@
 package com.sgr.glucomaster
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.userProfileChangeRequest
+import com.sgr.glucomaster.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    //Variables
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        val binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnCrearCuenta.setOnClickListener {
+
+            var name = binding.etRegisterName.text.toString()
+            var email = binding.etRegisterEmail.text.toString()
+            var pass = binding.etRegisterPass.text.toString()
+            var repeatPass = binding.etRegisterRepeatPass.text.toString()
+
+            //Check no empty fields
+            if (name.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && repeatPass.isNotEmpty()) {
+
+                //Check password length
+                if (pass.length > 5) {
+
+                    //Check two pass are the same
+                    if (pass.equals(repeatPass)) {
+
+                        createAccount(name, email, pass)
+                    }else {
+
+                        Toast.makeText(baseContext, getString(R.string.passnocoinciden), Toast.LENGTH_SHORT)
+                            .show()
+                        binding.etRegisterPass.requestFocus()
+                    }
+                } else {
+
+                    Toast.makeText(baseContext, getString(R.string.passmenor6), Toast.LENGTH_SHORT).show()
+                }
+            } else {
+
+                Toast.makeText(baseContext, getString(R.string.camposvacios), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    //Function to create an account
+    private fun createAccount(name: String, email: String, pass: String) {
+
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+
+                    //Get the user
+                    val user = auth.currentUser
+                    //Prepare updates to profile
+                    val profileUpdates = userProfileChangeRequest {
+                        displayName = name
+                    }
+                    //Update the profile
+                    user!!.updateProfile(profileUpdates)
+
+                    Toast.makeText(baseContext, getString(R.string.cuentacreada), Toast.LENGTH_SHORT)
+                        .show()
+                    finish()
+                } else {
+
+                    //Check if the email is in use
+                    val exception = it.exception
+                    if (exception is FirebaseAuthUserCollisionException){
+
+                        Toast.makeText(this, getString(R.string.correoenuso), Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+
+                        Toast.makeText(baseContext, "Error" + exception, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
     }
 }
