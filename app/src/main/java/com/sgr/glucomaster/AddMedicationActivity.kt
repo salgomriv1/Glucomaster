67985a280bf6d7b9ea67904e487c998f171d9bcb
salgomriv1 <salgomriv@alu.edu.gva.es>
2024-04-26 17:23:16 +0200

    Updated medications screen, designed change regimen screen

--- app/src/main/java/com/sgr/glucomaster/AddMedicationActivity.kt
+++ app/src/main/java/com/sgr/glucomaster/AddMedicationActivity.kt
@@ -1,20 +1,39 @@
 package com.sgr.glucomaster
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sgr.glucomaster.databinding.ActivityAddMedicationBinding
 
 class AddMedicationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddMedicationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_medication)
+        binding = ActivityAddMedicationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val spinnerTurn = binding.spAddMedTurn
+
+        //Data to show on spinner
+        val medicaciones = arrayOf("Desayuno", "Comida", "Cena", "Resopón")
+
+        //Create an adapter and a spinner layout
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, medicaciones)
+
+        //Specify the layout
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        //Asign the adapter to the spinner
+        spinnerTurn.adapter = adapter
     }
 }
--- app/src/main/java/com/sgr/glucomaster/CurrentRegimenActivity.kt
+++ app/src/main/java/com/sgr/glucomaster/CurrentRegimenActivity.kt
@@ -1,5 +1,6 @@
 package com.sgr.glucomaster
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.sgr.glucomaster.databinding.ActivityCurrentRegimenBinding
 
 class CurrentRegimenActivity : AppCompatActivity() {
 
@@ -15,11 +17,13 @@ class CurrentRegimenActivity : AppCompatActivity() {
         "100 - 999mg/l   Dosis: 3UD")
     lateinit var mRecyclerView: RecyclerView
     val mAdapter : MAdapter = MAdapter()
+    private lateinit var binding: ActivityCurrentRegimenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_current_regimen)
+        binding = ActivityCurrentRegimenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -30,6 +34,12 @@ class CurrentRegimenActivity : AppCompatActivity() {
         setupRecyclerView(R.id.tMRecyclerT, list1)
         setupRecyclerView(R.id.tMRecyclerC, list1)
         setupRecyclerView(R.id.tMRecyclerR, list1)
+
+        binding.btnCambiar.setOnClickListener {
+
+            val intent = Intent (this, ChangeRegimenActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun setupRecyclerView(Recycler: Int, list: MutableList<String>) {
